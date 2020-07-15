fdaabecd7126c35a6627fbb1077cb72a06ab1c06
ianhi <ianhuntisaak@gmail.com>
2020-07-14 22:54:59 -0400

    add pure websocket test

---
+++ pure-websocket-test/server.py
@@ -0,0 +1,57 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+from time import time
+import json
+import matplotlib.pyplot as plt
+from IPython import embed
+    
+do_embed = False
+times = []
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+    
+class SimpleWebSocket(tornado.websocket.WebSocketHandler):
+    connections = set()
+    
+    def open(self):
+        self.connections.add(self)
+    
+    async def on_message(self, message):
+        # print(message)
+        message = json.loads(message)
+        if message['what'] == 'time':
+            message['python_time'] = str(int(time()*1000))
+            [client.write_message(message) for client in self.connections]
+        else:
+
+            if 'manual' in message['what']:
+                # nonsense global stuff bc i couldn't get embed to work from here
+                global do_embed
+                global times
+                do_embed = True
+                global times
+                times = message['times']
+            else:
+                plt.hist(message['times'], bins='auto', density=True)
+                plt.xlabel('round trip (ms)')
+                plt.show()
+            tornado.ioloop.IOLoop.current().stop()
+    
+    def on_close(self):
+        self.connections.remove(self)
+    
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/websocket", SimpleWebSocket)
+    ])
+    
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(9999)
+    print('http://localhost:9999/')
+    tornado.ioloop.IOLoop.current().start()
+    if do_embed:
+        embed(colors='neutral')
